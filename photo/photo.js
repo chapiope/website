b97fb9cd2628bc88cde6e87292376eac5121a621
chapiope <genshiro111223@gmail.com>
2024-11-20 12:13:15 +0900

    Update photo.js

--- photo/photo.js
+++ photo/photo.js
@@ -30,3 +30,52 @@
 
     animObserver(photolight, -100);
 }
+
+const btn1 = document.querySelector('.btn-1');
+const modal1 = document.querySelector('.modal-1');
+const btn2 = document.querySelector('.btn-2');
+const modal2 = document.querySelector('.modal-2');
+const closeBtn1 = document.querySelector('.close-1');
+const closeBtn2 = document.querySelector('.close-2');
+const overlay = document.querySelector('.overlay');
+
+// ボタンをクリックしたら、モダルとオーバーレイに.activeを付ける
+btn1.addEventListener('click', function(e){
+  // aタグのデフォルトの機能を停止する
+  e.preventDefault();
+  　// モーダルとオーバーレイにactiveクラスを付与する
+  modal1.classList.add('active');
+  overlay.classList.add('active');
+});
+
+// モダルの閉じるボタンをクリックしたら、モダルとオーバーレイのactiveクラスを外す
+closeBtn1.addEventListener('click', function(){
+    modal1.classList.remove('active');
+    overlay.classList.remove('active');
+});
+  
+  // オーバーレイをクリックしたら、モダルとオーバーレイのactiveクラスを外す
+overlay.addEventListener('click', function() {
+    modal1.classList.remove('active');
+    overlay.classList.remove('active');
+});
+
+btn2.addEventListener('click', function(e){
+    // aタグのデフォルトの機能を停止する
+    e.preventDefault();
+    　// モーダルとオーバーレイにactiveクラスを付与する
+    modal2.classList.add('active');
+    overlay.classList.add('active');
+});
+
+// モダルの閉じるボタンをクリックしたら、モダルとオーバーレイのactiveクラスを外す
+closeBtn2.addEventListener('click', function(){
+  modal2.classList.remove('active');
+  overlay.classList.remove('active');
+});
+
+// オーバーレイをクリックしたら、モダルとオーバーレイのactiveクラスを外す
+overlay.addEventListener('click', function() {
+  modal2.classList.remove('active');
+  overlay.classList.remove('active');
+});
